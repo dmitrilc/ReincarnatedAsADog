ffaffe1cec41970562b1a70b894256ed080aa619
dmitrilc <dmitrilc@users.noreply.github.com>
2022-07-23 13:24:44 -0400

    Move classes to sensible packages

--- app/src/main/java/com/example/reincarnatedasadog/ui/fragment/PersonalityFragment.kt
+++ app/src/main/java/com/example/reincarnatedasadog/ui/fragment/PersonalityFragment.kt
@@ -1,11 +1,12 @@
-package com.example.reincarnatedasadog
+package com.example.reincarnatedasadog.ui.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.reincarnatedasadog.ui.viewmodel.PersonalityViewModel
+import com.example.reincarnatedasadog.R
 
 class PersonalityFragment : Fragment() {
 
