c65945e98626ee3f7c58df8c5f8e38ee480191aa
dmitrilc <dmitrilc@users.noreply.github.com>
2022-07-25 08:48:38 -0400

    Add Hilt configuration

--- app/src/main/java/com/example/reincarnatedasadog/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/reincarnatedasadog/ui/activity/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.reincarnatedasadog.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.reincarnatedasadog.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
