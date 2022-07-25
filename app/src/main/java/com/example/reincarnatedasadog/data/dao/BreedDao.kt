a0ebe0ca2ed11697fbffa5d8695718a93559b702
dmitrilc <dmitrilc@users.noreply.github.com>
2022-07-25 08:47:56 -0400

    Add Insert method

--- app/src/main/java/com/example/reincarnatedasadog/data/dao/BreedDao.kt
+++ app/src/main/java/com/example/reincarnatedasadog/data/dao/BreedDao.kt
@@ -1,6 +1,7 @@
 package com.example.reincarnatedasadog.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.example.reincarnatedasadog.data.entity.Breed
 
@@ -11,4 +12,7 @@ interface BreedDao {
 
     @Query("SELECT * FROM breed WHERE name LIKE :name LIMIT 1")
     fun findByName(name: String): Breed
+
+    @Insert
+    fun insert(breed: Breed)
 }
