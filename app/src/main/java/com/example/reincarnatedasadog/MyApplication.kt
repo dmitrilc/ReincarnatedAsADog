24cf11b25a22f68d0070279ee1b995daa764ecdb
dmitrilc <dmitrilc@users.noreply.github.com>
2022-07-25 08:42:41 -0400

    Add Worker configuration

--- app/src/main/java/com/example/reincarnatedasadog/MyApplication.kt
+++ app/src/main/java/com/example/reincarnatedasadog/MyApplication.kt
@@ -1,11 +1,25 @@
 package com.example.reincarnatedasadog
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.google.android.material.color.DynamicColors
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
-class MyApplication : Application() {
+@HiltAndroidApp
+class MyApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this);
     }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
