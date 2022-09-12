6f26ebd7a16b485e516e0805f3a48d0ff09f2080
dmitrilc <dmitrilc@users.noreply.github.com>
2022-09-12 14:30:04 -0400

    Update onCreateView

--- app/src/main/java/com/example/reincarnatedasadog/ui/fragment/PersonalityFragment.kt
+++ app/src/main/java/com/example/reincarnatedasadog/ui/fragment/PersonalityFragment.kt
@@ -24,7 +24,7 @@ class PersonalityFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPersonalityBinding.inflate(inflater)
 
         binding.buttonSubmit.setOnClickListener {
