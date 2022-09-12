9a7b9b565b1d7fdd75bd533755797bdc4d1fd150
dmitrilc <dmitrilc@users.noreply.github.com>
2022-09-12 14:29:02 -0400

    Set up DI for Moshi

---
+++ app/src/main/java/com/example/reincarnatedasadog/di/service/MoshiModule.kt
@@ -0,0 +1,19 @@
+package com.example.reincarnatedasadog.di.service
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MoshiModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .build()
+
+}
