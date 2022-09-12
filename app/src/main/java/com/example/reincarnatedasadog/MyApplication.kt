75b25a4f7582d976626d74b2717f75a7ca88377b
dmitrilc <dmitrilc@users.noreply.github.com>
2022-09-12 14:29:25 -0400

    Remove Worker configuration because we no longer need Workers

--- app/src/main/java/com/example/reincarnatedasadog/MyApplication.kt
+++ app/src/main/java/com/example/reincarnatedasadog/MyApplication.kt
@@ -1,25 +1,14 @@
 package com.example.reincarnatedasadog
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
+class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this);
     }
-
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
 }
